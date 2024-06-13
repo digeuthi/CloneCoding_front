a966535c3447470ff73987bc9f9dfb64f963e34b
Dahye Kim <digeuthi@gmail.com>
2024-06-13 15:59:44 +0900

    variables

--- javascript/nomard_1/app.js
+++ javascript/nomard_1/app.js
@@ -1,3 +1,11 @@
 // alert("hi");
 //console에 보내는 메시지
-console.log(45122)
+console.log(45122) //java의 print와 같은것
+
+const a = 5;//상수
+const b = 2;
+const myName = "d";
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("Hello my Name is " + myName);
