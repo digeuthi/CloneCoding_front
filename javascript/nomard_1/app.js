007a2c6ad774e8a89ff8c23474e4e850804a0d79
Dahye Kim <digeuthi@gmail.com>
2024-06-18 18:44:13 +0900

    boolean, null, undefined

--- javascript/nomard_1/app.js
+++ javascript/nomard_1/app.js
@@ -19,3 +19,11 @@ myName = "dd"; //let의 경우 추후 변경시에 let 다시 언급X, 새로운
 console.log("my new name is " + myName);
 
 //이전에는 var을 사용했지만, var을 사용하게되면 어떤걸 수정하지않을건지, 할건지를 구별하지 못하게 되서 사용하지않는다
+
+//Boolean
+const amIFat = null; //defined 상태, 값이 null
+let something; //undefined
+console.log(something);
+
+//javascript에는 nonexist, nondefinde, nothing 같은 타입도 존재
+//nothting의 경우 null에 해당! false != null , null은 아무것도 없는 상태
